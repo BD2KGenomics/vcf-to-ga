6670b1fac70d3c9352f9a163878c1966b548c495
Arc39 <kaspencer@csumb.edu>
2016-07-13 10:21:14 -0700

    adding everything

--- pbmes.py
+++ pbmes.py
@@ -4,29 +4,43 @@ import pysam
 from pysam import VariantFile
 import google.protobuf.json_format as json_format
 
-def varmes(rec):
+def vMes(rec):
 	gaVariant = variants_pb2.Variant()
 	gaVariant.id = rec.id
-	gaVariant.reference_name = rec.contig
-	#gaVariant.variant_set_id = rec.variant_set_id
-	#gaVariant.names
-	#gaVariant.created = rec.created
-	#gaVariant.updated = rec.updated
+	gaVariant.reference_name = rec.chrom
+	gaVariant.variant_set_id = 
+	#gaVariant.names =
+	#gaVariant.created = 
+	#gaVariant.updated = 
 	gaVariant.start = rec.start
 	gaVariant.end = rec.stop
 	gaVariant.reference_bases = rec.ref
 	if rec.alts is not None:
 		gaVariant.alternate_bases.extend(list(rec.alts))
-	#if rec.info is not None:
-		#gaVariant.info.append(list(rec.info))
+	#if info is not None:
+		#gaVariant.info.
 	#gaVariant.calls = rec.calls
 	return gaVariant
 
+def csMes(hdr):
+	gaCallSet = variants_pb2.CallSet()
+	gaCallSet.id = hdr.id
+	gaCallSet.name = sample
+	gaCallSet.bio_sample_id = sample
+
+
+	return gaCallSet
+
+#VCF file to read
 file = sys.argv[1]
+#file to output to
 ofile = sys.argv[2]
 sys.stdout = open(ofile, "w")
 samp = pysam.VariantFile(file)
-chrs = "ref_brca1"
-for rec in samp.fetch(chrs, 0, 100):
-	print (json_format._MessageToJsonObject(varmes(rec), True))
-
+hdr = samp.header
+#chromosome choice
+chromo = "ref_brca1"
+for rec in samp.fetch(chromo, 0, 100):
+	print (json_format._MessageToJsonObject(vMes(rec), True))
+for sample in hdr.samples:
+	print (json_format._MessageToJsonObject(csmes(hdr), True))
